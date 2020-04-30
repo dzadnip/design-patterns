0c5e573cbb356aac46271285b298e6df4f98f05c
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-29 18:31:39 -0700

    Added Builder design pattern

---
+++ Creational/Builder/IBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    // The Builder interface specifies methods for creating the different parts
+    // of the Product objects.
+    interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+}
