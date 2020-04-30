54d47a299312d4baee547d0d710b306cb9d6d41c
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-29 18:38:19 -0700

    Added Adapter design pattern

---
+++ Structural/Adapter/ITarget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    // The Target defines the domain-specific interface used by the client code.
+    interface ITarget
+    {
+        string GetRequest();
+    }
+}
