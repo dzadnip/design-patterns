e017315af568ad8eb79518a20c485c7db60a6254
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-29 18:29:02 -0700

    Added AbstractFactory design pattern

---
+++ Creational/AbstractFactory/ConcreteFactory2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    // Concrete Factories produce a family of products that belong to a single
+    // variant. The factory guarantees that resulting products are compatible.
+    // Note that signatures of the Concrete Factory's methods return an abstract
+    // product, while inside the method a concrete product is instantiated.
+    // Each Concrete Factory has a corresponding product variant
+    class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+}
