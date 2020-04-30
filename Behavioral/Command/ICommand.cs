367ede167138edfcfab3f486083f9492b291e5c7
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-29 19:13:31 -0700

    Added Command design pattern

---
+++ Behavioral/Command/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command
+{
+    // The Command interface declares a method for executing a command.
+    interface ICommand
+    {
+        void Execute();
+    }
+}
